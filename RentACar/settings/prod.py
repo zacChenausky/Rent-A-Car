a9f6959efe5d7a4a403dbc266e5bf9f6864dca17
Zac <ZacharyChenausky@my.unt.edu>
2022-07-13 09:38:25 -0500

    test bug fix

--- RentACar/settings/prod.py
+++ RentACar/settings/prod.py
@@ -7,35 +7,35 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = False
 ALLOWED_HOST = [
-  'rentacar.herokuapp.com',
+    'rentacar-web.herokuapp.com',
 ]
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 LOGGING = {
-  'version': 1,
-  'disable_existing_loggers': False,
-  'formatters': {
-    'verbose': {
-      'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-      'datefmt' : "%d/%b/%Y %H:%M:%S"
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
     },
-    'simple': {
-      'format' : '%(levelname)s %(message)s'
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
     },
-  },
-  'handlers': {
-    'console': {
-      'level': 'DEBUG',
-      'class': 'logging.StreamHandler',
-    },
-  },
-  'loggers': {
-    'MYAPP': {
-      'handlers': ['console'],
-      'level' : 'DEBUG',
-    },
-  }
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
 }
 
 
